ceb0e47fad52728965c36ac6c9be7b22bae5aedb
Edgy Corner <siddharthlohchab12@gmail.com>
2018-05-22 05:27:07 -0700

    lel

--- add.php
+++ add.php
@@ -7,7 +7,7 @@ echo "test";
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
 $MadelineProto->start();
 
-$ch=-1222258609;
+$ch="-1222258609";
 $u='@lewisgarden';
 $Updates = $MadelineProto->channels->inviteToChannel(['channel' => $ch, 'users' => [$u], ]); 
 echo "done";
