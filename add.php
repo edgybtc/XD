18c3229e5346b89c41245060157051a590a0c7e7
Edgy Corner <siddharthlohchab12@gmail.com>
2018-05-22 06:29:39 -0700

    ss

--- add.php
+++ add.php
@@ -8,7 +8,7 @@ $MadelineProto = new \danog\MadelineProto\API('session.madeline');
 $MadelineProto->start();
 
 $ch='https://t.me/joinchat/G2J3wUjaL7Fo-CRz6sLsKQ';
-$u='@shaktiman';
+$u='445';
 $Updates = $MadelineProto->channels->inviteToChannel(['channel' => $ch, 'users' => [$u], ]); 
 echo "done";
 echo $Updates;
