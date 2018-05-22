15e060cce823c3ad99324e8b0c8fe3a5429e9026
Edgy Corner <siddharthlohchab12@gmail.com>
2018-05-22 04:48:31 -0700

    changed

--- index.php
+++ index.php
@@ -13,14 +13,12 @@ $me = $MadelineProto->get_self();
 \danog\MadelineProto\Logger::log($me);
 
 if (!$me['bot']) {
-    $MadelineProto->messages->sendMessage(['peer' => '@danogentili', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
-    $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+    $MadelineProto->messages->sendMessage(['peer' => '@Subex', 'message' => "I LOVE YOU! <3"]);
+   
 
-    try {
-        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
-    } catch (\danog\MadelineProto\RPCErrorException $e) {
+    catch (\danog\MadelineProto\RPCErrorException $e) {
     }
 
-    $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
+    $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/G2J3wUjaL7Fo-CRz6sLsKQ', 'message' => 'Testing MadelineProto!']);
 }
 echo 'OK, done!'.PHP_EOL;
